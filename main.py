ddc4c06bd253b2cf9dd4fc29a8e39f6f48bed815
miracle3456 <miraclepayton123@gmail.com>
2025-03-19 18:45:27 +0300

    added a menu

---
+++ main.py
@@ -0,0 +1,15 @@
+def menu():
+    print("1.Add item")
+    print("2.Remove item")
+    print("3.View items")
+    print("4.Update item")
+    print("5.Clear stock")
+    print("6.Exit")
+
+
+
+
+def main():
+    items = {}
+    choice = input("Enter your choice: ")
+    
